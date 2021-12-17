df14577bddd85214adff686291fb1acbe2a0b58a
sogrey <suzhaoheng2006@163.com>
2021-12-18 01:27:51 +0800

    update .

---
+++ src/components/MonacoEditor.vue
@@ -0,0 +1,19 @@
+<script setup>
+import * as monaco from "monaco-editor";
+
+defineProps({
+  //   msg: String
+});
+
+this.monacoInstance = monaco.editor.create(this.$refs["container"], {
+  value: this.value,
+  ...this.options,
+});
+</script>
+
+<template>
+  <div class="the-code-editor-container" ref="container"></div>
+</template>
+
+<style scoped>
+</style>
